d30a06d10a32e318e5a171c2dfc89beedb83ec1b
antonlabz <20198370+antonlabz@users.noreply.github.com>
2019-09-01 13:48:12 +1000

    Added missing flair IDs

--- js/flairs.js
+++ js/flairs.js
@@ -10,7 +10,7 @@ loadPage = function() {
 		var enter = document.getElementById('content');
 
 		//replace with latest unit number
-		for (i=1; i<=2639; i++) {
+		for (i=1; i<=2682; i++) {
 			unit_id = i;
 
 			if (i>=1 && i<=800) {
